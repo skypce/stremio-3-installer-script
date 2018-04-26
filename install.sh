6cfb83fe273ced192c7c2a7cd8a929ec2028b294
César R Cid Mendez <skypce@gmail.com>
2018-04-26 14:04:09 -0300

    c

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 curl -SO# http://dl.strem.io/Stremio3.6.5.linux.tar.gz
 mkdir -p /opt/stremio
 tar -xvzf Stremio3.6.5.linux.tar.gz -C /opt/stremio
-curl -SO# https://icon-icons.com/icons2/1381/PNG/512/stremio_94899.png
-mv stremio_94899.png /opt/stremio/
+curl -SO# https://raw.githubusercontent.com/skypce/stremio-3-installer-script/master/stremio.svg
+mv stremio.svg /opt/stremio/
 curl -SO# https://raw.githubusercontent.com/skypce/stremio-3-installer-script/master/stremio.desktop
 mv stremio.desktop /usr/share/applications/
+rm Stremio3.6.5.linux.tar.gz
