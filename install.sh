b1f0d4bc8b92fc37f299add3c75185114e475ad3
César R Cid Mendez <skypce@gmail.com>
2018-04-26 13:51:56 -0300

    desktop file

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ mkdir -p /opt/stremio
 tar -xvzf Stremio3.6.5.linux.tar.gz -C /opt/stremio
 curl -SO# https://icon-icons.com/icons2/1381/PNG/512/stremio_94899.png
 mv stremio_94899.png /opt/stremio/
-curl -SO# https://gist.githubusercontent.com/claudiosmweb/797b502bc095dabee606/raw/52ad06b73d90a4ef389a384fbc815066c89798eb/stremio.desktop
+curl -SO# https://raw.githubusercontent.com/skypce/stremio-3-installer-script/master/stremio.desktop
 mv stremio.desktop /usr/share/applications/
